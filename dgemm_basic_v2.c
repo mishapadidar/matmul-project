2f9a2021f7c5e42cc6a16ff469a3d0bddc791138
Darian Nwankwo <nwankwodarian@gmail.com>
2020-10-03 15:49:41 -0400

    generalized SLICE size

--- dgemm_basic_v2.c
+++ dgemm_basic_v2.c
@@ -1,7 +1,13 @@
 /*
 breaking up loops for vectorization
 */
+#include <stdlib.h>
+
 const char* dgemm_desc = "Basic, three-loop dgemm.";
+
+#ifndef SLICE
+#define SLICE ((int) 8)
+#endif
 
 void square_dgemm(const int M, 
                   const double *A, const double *B, double *C)
@@ -10,14 +16,20 @@ void square_dgemm(const int M,
     for (i = 0; i < M; ++i) {
         for (j = 0; j < M; ++j) {
             // double cij = C[j*M+i];
-            double s[4] = {0, 0, 0, 0};
-            for (k = 0; k < M-3; k+=4) {
-                for (l=0; l < 4; ++l) {
+            double *s = (double *) malloc(sizeof(double) * SLICE);
+            for (int ndx = 0; ndx < SLICE; ndx++) {
+                s[ndx] = 0.0;
+            }
+
+            for (k = 0; k < M-(SLICE-1); k+=SLICE) {
+                for (l=0; l < SLICE; ++l) {
                     s[l] += A[(k+l)*M+i] * B[j*M+k+l];
                 }
             }
             // Combine sub-sums and handle trailing elements
-            double cij = (s[0]+s[1]) + (s[2]+s[3]);
+            double cij = 0.0;
+            for (int ndx = 0; ndx < SLICE; ndx++)
+                cij += s[ndx];
             for (; k < M; ++k)
                 cij += A[k*M+i] * B[j*M+k];
             C[j*M+i] = cij;
